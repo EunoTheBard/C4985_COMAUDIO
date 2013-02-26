33e2a7b02f005f42fdef949c19ac632e8db8e4bc
Nick Raposo <nickraposo17@gmail.com>
2013-02-26 11:09:08 -0800

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,34 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#define BUFFSIZE 8192
+#define GAPSIZE 15
+#define TCP_DEFAULT_PORT 6942
+#define ID_TIMER 49999
+
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+VOID CALLBACK TimerProc (HWND, UINT, UINT, DWORD);
+DWORD WINAPI Connect ( LPVOID ); 
+DWORD WINAPI ConnectUDP ( LPVOID ); 
+DWORD WINAPI TCP_SERVER ( LPVOID );
+DWORD WINAPI AcceptThread ( LPVOID );
+void CALLBACK TCPRoutine( DWORD, DWORD, LPWSAOVERLAPPED, DWORD );
+void CALLBACK UDPRoutine( DWORD, DWORD, LPWSAOVERLAPPED, DWORD );
+
+void stopTimer( void );
+void fatal ( char* );
+void non_fatal( char* );
+
+typedef struct _SOCKET_INFORMATION {
+   OVERLAPPED Overlapped;
+   SOCKET Socket;
+   CHAR Buffer[8192];
+   WSABUF DataBuf;
+   DWORD BytesSEND;
+   DWORD BytesRECV;
+} SOCKET_INFORMATION, * LPSOCKET_INFORMATION;
+
+
+#endif
