fb3387260ff43f96b3784ba64033a67511eba94e
aikorose <aikorose7@gmail.com>
2013-03-17 10:46:46 -0700

    Update resource.h

--- UDPClient/resource.h
+++ UDPClient/resource.h
@@ -3,33 +3,35 @@
 // Used by Player.rc
 //
 #define IDR_MENU1                       101
-#define IDC_TRACKBAR  				102
-#define ID_OPEN							103
-#define	ID_PLAY							104
-#define	ID_PAUSE						105
-#define	ID_STOP							106
-#define	ID_LOOP							107
-#define	ID_DOWN							108
-#define	ID_UP							109
-#define ID_EXITT						110
-#define ID_TAB							111
-#define ID_INFO1						112
-#define ID_INFO2						113
-#define ID_INFO3						114
-#define ID_INFO4						115
-#define ID_STATICWND					116
-#define ID_STATIC						117
-#define ID_PAINT2						118
+#define IDC_TRACKBAR                    102
+#define ID_OPEN                         103
+#define ID_PLAY                         104
+#define ID_PAUSE                        105
+#define ID_STOP                         106
+#define ID_LOOP                         107
+#define ID_DOWN                         108
+#define ID_UP                           109
+#define ID_EXITT                        110
+#define ID_TAB                          111
+#define ID_OPEN2                        112
+#define ID_PLAY2                        113
+#define ID_PAUSE2                       114
+#define ID_STOP2                        115
+#define ID_EXITT2                       116
+#define ID_STATICWND                    117
+#define ID_STATIC                       118
 #define ID_OPTIONS_DOWNLOADMUSIC        40001
-#define ID_OPTIONS_MICROPHONE			40002
-#define ID_EXIT_EXIT                    40003
-					
+#define ID_OPTIONS_MICROPHONE           40002
+#define ID_DIRECTX_MULTIPLAYER          40003
+#define ID_DIRECTX_TABCLOSEFORMULTIPLAYER 40004
+#define ID_EXIT_EXIT                    40005
+
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        119
-#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_COMMAND_VALUE         40006
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
