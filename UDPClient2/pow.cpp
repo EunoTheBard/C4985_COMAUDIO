3a4701f3009923a7124908242f69fc5565465d6f
aikorose <aikorose7@gmail.com>
2013-03-26 02:09:43 -0700

    Create pow.cpp

---
+++ UDPClient2/pow.cpp
@@ -0,0 +1,30 @@
+/*------------------------------------------------------------------------------------------------------------------
+--  		FUNCTION:		pow
+--
+--			DATE:			FEB 28, 2013
+--
+--			REVISIONS:		Mar, 2013
+--
+--			DESIGNER:		Nicholas Raposo	(Team Leader)
+--			    &			Christopher Porter
+--			PROGRAMMER:	    Cody Srossiter
+--							Aiko Rose	
+--
+--			INTERFACE:		int pow(int base,int num)
+--
+--			RETURNS:		
+--
+--			NOTES:			
+							 
+--
+----------------------------------------------------------------------------------------------------------------------*/
+
+#include <windows.h>
+
+int pow(int base,int num)	
+{
+	int sum=1;
+	for(int i=0;i<num;i++)
+		sum*=base;
+	return sum;
+}
