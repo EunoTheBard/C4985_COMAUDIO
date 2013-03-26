4c91ff19ac7deb53dad5eb3402488c39851a7b22
aikorose <aikorose7@gmail.com>
2013-03-26 02:11:45 -0700

    Create resource.h

---
+++ UDPClient2/resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Player.rc
+//
+#define IDR_MENU1                       101
+#define IDC_TRACKBAR                    102
+#define ID_OPEN                         103
+#define ID_PLAY                         104
+#define ID_PAUSE                        105
+#define ID_STOP                         106
+#define ID_LOOP                         107
+#define ID_DOWN                         108
+#define ID_UP                           109
+#define ID_EXITT                        110
+#define ID_TAB                          111
+#define IDD_DIALOG1                     112
+#define IDD_DIALOG2                     113
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_EDIT3                       1003
+#define IDC_EDIT4                       1004
+#define ID_OPTIONS_DOWNLOADMUSIC        40001
+#define ID_OPTIONS_MICROPHONE           40002
+#define ID_DIRECTX_MULTIPLAYER          40003
+#define ID_EXIT_EXIT                    40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        114
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
