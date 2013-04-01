1e0348c4d24311ea774075c5269b6ac39c16fea1
aikorose <aikorose7@gmail.com>
2013-03-31 20:26:29 -0700

    Update WndProc.cpp

--- UDPClient2/WndProc.cpp
+++ UDPClient2/WndProc.cpp
@@ -26,6 +26,17 @@
 
 void createSocketForMulticast(HWND);
 void createSocketForMicrophone(HWND);
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK DlgProc2(HWND, UINT, WPARAM, LPARAM);
+
+static HWND hDlg = NULL;
+static HWND hDlg2 = NULL;
+static u_short port;
+static char szTo[80];
+static char szTo2[80];
+static u_long lMCAddr;
+static char achMCAddr[MAXADDRSTR];
+
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp){
 
@@ -819,6 +830,123 @@ void createSocketForMicrophone(HWND hWnd)
 	
     
 		MessageBox(hWnd, _T("Waiting for client's connection"), _T("UDP Client"), MB_OK);
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  	static HWND hParent;
+	BOOL bSuccess;
+	static HWND hSendto;
+	TCHAR errmsg[512];
+
+	hParent = GetParent(hwnd);
+	
+	switch(uMsg)
+	{
+		case WM_INITDIALOG:
+			hSendto = GetDlgItem(hDlg, IDC_EDIT1);
+			SetDlgItemText(hwnd, IDC_EDIT1, TIMECAST_ADDR);
+			SetDlgItemInt(hwnd, IDC_EDIT2, TIMECAST_PORT, FALSE);
+
+		r	eturn TRUE;
+		case WM_COMMAND:
+
+		switch(LOWORD(wParam))
+		{	
+			case IDOK:
+				GetWindowText(hSendto, szTo, (int)sizeof(szTo));
+				if(strcmp(szTo, "") == 0)
+				{
+					MessageBox(hDlg, _T("no server address"), _T("UDP Client"), MB_OK);
+					SetFocus(GetDlgItem(hwnd, IDC_EDIT1));
+					return FALSE;
+				}
+				else
+				{
+					/* Evaluate multicast address provided */
+					lMCAddr = inet_addr(szTo);
+					if (!((lMCAddr>=0xe0000000) || (lMCAddr<=0xefffffff)))
+					{
+						strcpy(achMCAddr, TIMECAST_ADDR);
+						_stprintf(errmsg, _T("Invalid address %s provided by user.\nusing default address: %s"), szTo, achMCAddr);
+						MessageBox(hwnd, errmsg, "UDP Client", MB_OK);
+					}
+					else
+					{	
+						strcpy (achMCAddr, szTo);
+					}
+				}
+
+				if((port = (u_short)GetDlgItemInt(hwnd, IDC_EDIT2, &bSuccess, FALSE)) == 0)
+				{
+					MessageBox(hwnd, "Enter port number!", "UDP Client", MB_OK);
+					SetFocus(GetDlgItem(hwnd, IDC_EDIT2));
+					return FALSE;
+				}
+
+				PostMessage(hParent, WM_SOCKET_CREATE, 0, 0);
+				DestroyWindow(hwnd);
+				return TRUE;
+			case IDCANCEL:
+				DestroyWindow(hwnd);
+				return TRUE;
+		}
+			return FALSE;
+		case WM_DESTROY:
+			hDlg = NULL;
+			return TRUE;
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK DlgProc2(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hParent2;
+	BOOL bSuccess;
+	static HWND hSendto2;
+
+	hParent2 = GetParent(hwnd);
+	
+	switch(uMsg)
+	{
+		case WM_INITDIALOG:
+			hSendto2 = GetDlgItem(hDlg, IDC_EDIT3);
+			SetDlgItemInt(hwnd, IDC_EDIT4, DEFAULT_PORT, FALSE);
+
+			return TRUE;
+		case WM_COMMAND:
+
+			switch(LOWORD(wParam))
+			{	
+				case IDOK:
+					GetWindowText(hSendto2, szTo2, (int)sizeof(szTo2));
+					if(strcmp(szTo2, "") == 0)
+					{
+						MessageBox(hDlg, _T("no server address"), _T("UDP Client"), MB_OK);
+						SetFocus(GetDlgItem(hwnd, IDC_EDIT3));
+						return FALSE;
+					}
+					
+					if((port = (u_short)GetDlgItemInt(hwnd, IDC_EDIT4, &bSuccess, FALSE)) == 0)
+					{	
+						MessageBox(hwnd, _T("Enter port number!"), _T("UDP Client"), MB_OK);
+						SetFocus(GetDlgItem(hwnd, IDC_EDIT4));
+						return FALSE;
+					}
+
+				PostMessage(hParent2, WM_SOCKET_CREATE2, 0, 0);
+				DestroyWindow(hwnd);
+				return TRUE;
+				case IDCANCEL:
+					DestroyWindow(hwnd);
+					return TRUE;
+			}
+			return FALSE;
+		case WM_DESTROY:
+			hDlg2 = NULL;
+			return TRUE;
+	}
+	return FALSE;
 }
 
 
